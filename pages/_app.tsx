802437d9cba0a9434ebd67ed6e823b5305807a47
Vineel Sai <mail@vineelsai.com>
2022-12-31 13:50:42 +0530

    pagination and remove server sideprops in index

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,5 +4,5 @@ import type { AppProps } from 'next/app'
 
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />
+    return <Component {...pageProps} />
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,51 +1,68 @@
 import Head from 'next/head'
-import mongoose from 'mongoose'
-import Articles from '../models/article'
 import { ArticleType } from '../types/article'
 import Link from 'next/link'
 import Footer from '../src/Footer/Footer'
 import Navbar from '../src/Navbar/Navbar'
 import Image from 'next/image'
+import { useEffect, useState } from 'react'
+import Pagination from '../src/Pagination/Pagination'
 
-export default function Home({ articles }: { articles: ArticleType[] }) {
-	return (
-		<div>
-			<Head>
-				<title>Vineel Sai | Blog</title>
-				<meta name="description" content="Blog by Vineel Sai"></meta>
-				<link rel="icon" href="/logo.png" />
-			</Head>
+export default function Home() {
+    const [articles, setArticles] = useState<ArticleType[]>([])
+    const [page, setPage] = useState(0)
+    const [pageLimit, setPageLimit] = useState(10)
+    const [pageCount, setPageCount] = useState(0)
+    useEffect(() => {
+        const getArticles = async () => {
+            const res = await fetch('/api/articles', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    page: page,
+                    pageLimit: pageLimit
+                })
+            })
+            if (res.ok) {
+                const data = await res.json()
+                setArticles(data.articles)
+                setPageCount(data.pageCount)
+            } else {
+                console.log('error')
+            }
+        }
+        getArticles()
+    }, [page, pageLimit])
+    return (
+        <div>
+            <Head>
+                <title>Vineel Sai | Blog</title>
+                <meta name="description" content="Blog by Vineel Sai"></meta>
+                <link rel="icon" href="/logo.png" />
+            </Head>
 
-			<Navbar />
+            <Navbar />
 
-			<main className='main'>
-				{
-					articles.map(article => (
-						<Link href={"post/" + article.url} className='card' key={article.url}>
-							<Image src={article.imageUrl} className='image' alt="image" width={50} height={50} />
-							<h3>{article.title}</h3>
-							<p>{article.description.substring(0, 100)}</p>
-						</Link>
-					))
-				}
-			</main>
+            <div className='main'>
+                {
+                    articles.map(article => (
+                        <Link href={"post/" + article.url} className='card' key={article.url}>
+                            <Image src={article.imageUrl} className='image' alt="image" width={50} height={50} />
+                            <h3>{article.title}</h3>
+                            <p>{article.description.substring(0, 100)}</p>
+                        </Link>
+                    ))
+                }
+            </div>
 
-			<Footer />
-		</div >
-	)
-}
+            <Pagination
+                pageCount={pageCount}
+                setPage={setPage}
+                page={page}
+            />
 
-export async function getServerSideProps() {
-	try {
-		mongoose.connect(process.env.NEXT_MONGODB_URL!)
-		console.log("connected")
-	} catch (error) {
-		console.log(error)
-	}
-	const articles = await Articles.find()
-	return {
-		props: {
-			articles: JSON.parse(JSON.stringify(articles)) as ArticleType[]
-		}
-	}
+            <Footer />
+        </div >
+    )
 }
