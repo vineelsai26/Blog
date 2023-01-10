057dc170996b2ecc4b1bedd7729766aea2124463
Vineel Sai <mail@vineelsai.com>
2023-01-10 23:47:22 +0530

    fix build error

--- src/Footer/Footer.tsx
+++ src/Footer/Footer.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 export default function Footer() {
 	return (
 		<footer className="flex overflow-hidden right-0 left-0 p-4 bottom-0 bg-white shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
@@ -5,7 +7,7 @@ export default function Footer() {
 			</span>
 			<ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
 				<li>
-					<a href="/contact" className="mr-4 hover:underline md:mr-6 ">Contact</a>
+					<Link href="/contact" className="mr-4 hover:underline md:mr-6 ">Contact</Link>
 				</li>
 			</ul>
 		</footer>
