ac9dc4fad0fcd0a2c3e2ffb38b2c072fc7f96087
Vineel Sai <mail@vineelsai.com>
2023-07-25 13:35:30 +0530

    add Highlight monitoring

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,8 @@ import { useEffect, useState } from 'react'
 import Footer from '../src/Footer/Footer'
 import Navbar from '../src/Navbar/Navbar'
 import '../styles/globals.css'
+import { HighlightInit } from '@highlight-run/next/highlight-init'
+
 
 const kalam = Kalam({
 	weight: '400',
@@ -38,19 +40,30 @@ export default function RootLayout({
 	}, [])
 
 	return (
-		<html lang='en'>
-			<head>
-				<link rel="icon" href={faviconHref} type="image/x-icon" />
-			</head>
-			<body className={`${kalam.className} bg-slate-200 dark:bg-gray-600`}>
-				<div className='sticky top-0 z-50 w-full'>
-					<Navbar />
-				</div>
-				<div>
-					{children}
-				</div>
-				<Footer />
-			</body>
-		</html>
+		<>
+			<HighlightInit
+				projectId={'2d1x54gr'}
+				tracingOrigins
+				networkRecording={{
+					enabled: true,
+					recordHeadersAndBody: true,
+					urlBlocklist: [],
+				}}
+			/>
+			<html lang='en'>
+				<head>
+					<link rel="icon" href={faviconHref} type="image/x-icon" />
+				</head>
+				<body className={`${kalam.className} bg-slate-200 dark:bg-gray-600`}>
+					<div className='sticky top-0 z-50 w-full'>
+						<Navbar />
+					</div>
+					<div>
+						{children}
+					</div>
+					<Footer />
+				</body>
+			</html>
+		</>
 	)
 }
