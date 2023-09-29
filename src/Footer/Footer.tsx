f1f4229464f90da3587fc33bb52a9e6e893f1932
Vineel Sai <mail@vineelsai.com>
2023-09-29 23:51:33 +0530

    update dependencies

--- src/Footer/Footer.tsx
+++ src/Footer/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
 	return (
 		<footer className='right-0 left-0 bottom-0 flex overflow-hidden bg-white p-4 shadow dark:bg-gray-800 md:flex md:items-center md:justify-between md:p-6'>
 			<span className='text-sm text-gray-500 dark:text-gray-400 sm:text-center'>
-				© 2022{' '}
+				© 2023{' '}
 				<a href='https://vineelsai.com/' className='hover:underline'>
 					Vineel Sai
 				</a>
