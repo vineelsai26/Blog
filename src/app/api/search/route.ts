8735c48152789b65499c0c55c62cb418f5c0cded
Vineel Sai <mail@vineelsai.com>
2023-10-11 21:33:05 +0530

    add tabs in article editor

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -1,10 +1,30 @@
 import prisma from '../../../prisma/prisma'
 import { NextRequest, NextResponse } from 'next/server'
+import { cookies } from 'next/headers'
+import bcrypt from 'bcrypt'
+
 
 export async function GET(req: NextRequest) {
 	const { searchParams } = new URL(req.url)
+
+	const cookieStore = cookies()
+	const email = cookieStore.get('email')?.value
+	const password = cookieStore.get('password')?.value
+	let result = false
+
+	if (email && password) {
+		const user = await prisma.users.findUnique({
+			where: {
+				email: email,
+			},
+		})
+
+		result = await bcrypt.compare(password, user!.password)
+	}
+
 	const articles = await prisma.articles.findMany({
 		where: {
+			private: result,
 			OR: [
 				{
 					title: {
