2b579fbd50611fe34705bd1d2fa60a4519488b9d
Vineel Sai <mail@vineelsai.com>
2024-05-11 14:00:39 +0530

    update deps, blur + animation on project cards

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from 'drizzle-kit'
 
 export default {
+	dialect: 'sqlite',
 	schema: 'src/drizzle/schema/*',
 	out: '.drizzle',
 	breakpoints: true,
--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -16,10 +16,13 @@ export default function ProjectCard({ project }: { project: ProjectType }) {
 					setShowButtons(false)
 				}}
 			>
-				<div className='absolute top-1/2 z-10 flex w-full -translate-y-1/2 flex-row justify-evenly p-4'>
+				<div
+					className='absolute top-1/2 z-10 flex w-full -translate-y-1/2 flex-row justify-evenly p-4'
+					hidden={!showButtons}
+				>
 					{project.downloadUrl && (
 						<button
-							className='relative w-2/5 rounded border border-blue-500 bg-transparent px-4 py-2 font-semibold text-blue-700 duration-300 hover:border-transparent hover:bg-blue-500 hover:text-white'
+							className='relative w-2/5 rounded border border-blue-500 bg-blue-500 bg-transparent px-4 py-2 font-semibold text-white duration-300 hover:border-blue-200 hover:border-transparent hover:bg-blue-700'
 							onClick={() =>
 								window.open(project.downloadUrl, '_blank')
 							}
@@ -30,7 +33,7 @@ export default function ProjectCard({ project }: { project: ProjectType }) {
 					)}
 					{project.previewUrl && (
 						<button
-							className='relative w-2/5 rounded border border-blue-500 bg-transparent px-4 py-2 font-semibold text-blue-700 duration-300 hover:border-transparent hover:bg-blue-500 hover:text-white'
+							className='relative w-2/5 rounded border border-blue-500 bg-blue-500 bg-transparent px-4 py-2 font-semibold text-white duration-300 hover:border-blue-200 hover:border-transparent hover:bg-blue-700'
 							onClick={() =>
 								window.open(project.previewUrl, '_blank')
 							}
@@ -40,7 +43,7 @@ export default function ProjectCard({ project }: { project: ProjectType }) {
 						</button>
 					)}
 					<button
-						className='relative w-2/5 rounded border border-blue-500 bg-transparent px-4 py-2 font-semibold text-blue-700 duration-300 hover:border-transparent hover:bg-blue-500 hover:text-white'
+						className='relative w-2/5 rounded border border-blue-500 bg-blue-500 bg-transparent px-4 py-2 font-semibold text-white duration-300 hover:border-blue-200 hover:border-transparent hover:bg-blue-700'
 						onClick={() => window.open(project.gitUrl, '_blank')}
 						hidden={!showButtons}
 					>
@@ -52,7 +55,10 @@ export default function ProjectCard({ project }: { project: ProjectType }) {
 					alt={project.name}
 					fill
 					sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
-					className='relative h-full w-full rounded-t-md object-cover'
+					className={
+						'relative h-full w-full rounded-t-md object-cover' +
+						(showButtons ? ' animate-pulse blur' : '')
+					}
 				/>
 			</div>
 			<div className='p-2'>
