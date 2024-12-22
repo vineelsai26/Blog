b36b9dd49ddb7de2e625303ad4f5813ef4c2fded
Vineel Sai <mail@vineelsai.com>
2024-12-22 17:39:53 +0530

    update drizzle version

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,10 @@
 import type { Config } from 'drizzle-kit'
 
 export default {
-	dialect: 'sqlite',
+	dialect: 'turso',
 	schema: 'src/drizzle/schema/*',
 	out: '.drizzle',
 	breakpoints: true,
-	driver: 'turso',
 	dbCredentials: {
 		url: process.env.NEXT_TURSO_URL!,
 		authToken: process.env.NEXT_TURSO_TOKEN!,
